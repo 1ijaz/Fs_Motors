3d122e7688b31b74554e040c6b0cb385e5addca9
Ijaz Muhammad <ijaz_muhammad@global.komatsu>
2023-07-29 15:43:05 +0500

    remove comments

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import './index.css';
 
 // scroll bar
 import 'simplebar/src/simplebar.css';
-//test commnet
 // third-party
 import { Provider as ReduxProvider } from 'react-redux';
 
@@ -40,7 +39,4 @@ root.render(
   </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
