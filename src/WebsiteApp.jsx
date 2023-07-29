25878ad374f926d1df6a9b6391f2b2bfdd95a42c
Ijaz Muhammad <ijaz_muhammad@global.komatsu>
2023-07-29 15:27:08 +0500

    Add website template

---
+++ src/WebsiteApp.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+import { Header } from 'website-components/header';
+import { Features } from 'website-components/features';
+import { About } from 'website-components/about';
+import { Services } from 'website-components/services';
+import { Gallery } from 'website-components/gallery';
+import { Testimonials } from 'website-components/testimonials';
+import { Team } from 'website-components/Team';
+import { Contact } from 'website-components/contact';
+import JsonData from './data/data.json';
+import SmoothScroll from 'smooth-scroll';
+import './App.css';
+import { Navigation } from 'website-components/navigation';
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true
+});
+
+const WebsiteApp = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+  return (
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      <Features data={landingPageData.Features} />
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
+      <Gallery data={landingPageData.Gallery} />
+      <Testimonials data={landingPageData.Testimonials} />
+      <Team data={landingPageData.Team} />
+      <Contact data={landingPageData.Contact} />
+    </div>
+  );
+};
+
+export default WebsiteApp;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+//website imports
+import React from 'react';
+import './index.css';
 
 // scroll bar
 import 'simplebar/src/simplebar.css';
@@ -15,18 +18,25 @@ import 'assets/third-party/apex-chart.css';
 import App from './App';
 import { store } from 'store';
 import reportWebVitals from './reportWebVitals';
+import WebsiteApp from 'WebsiteApp';
 
 // ==============================|| MAIN - REACT DOM RENDER  ||============================== //
-
+//const [isLogin, setIsLogin] = useState(false);
+let isLogin = false;
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
+//if islogin false
 root.render(
   <StrictMode>
-    <ReduxProvider store={store}>
-      <BrowserRouter basename="/">
-        <App />
-      </BrowserRouter>
-    </ReduxProvider>
+    {isLogin ? (
+      <ReduxProvider store={store}>
+        <BrowserRouter basename="/">
+          <App />
+        </BrowserRouter>
+      </ReduxProvider>
+    ) : (
+      <WebsiteApp />
+    )}
   </StrictMode>
 );
 
